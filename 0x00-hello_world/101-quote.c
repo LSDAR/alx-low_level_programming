10228f136b87e23eba9e8eacdba556fe3cb6d507
LSDAR <gdarko6tin@gmail.com>
2023-02-22 03:54:22 -0800

    loops repo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,3 @@
 { #include<stdio.h>
-    int main(void) { write(2, "and that piece of art is useful" -Dora Kopar, 2015-10-19\n",61);
+    int main(void) { write(2, "and that piece of art is useful" -Dora Kopar, 2015-10-19\n",61);}
+}
